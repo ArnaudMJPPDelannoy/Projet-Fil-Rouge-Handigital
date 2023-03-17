a5f5e2a359eb6095177c71593b18a6a915e38bbe
Arnaud DELANNOY <arnauddel74@gmail.com>
2023-03-17 12:01:45 +0100

    Made the style and footer work on my own phone's display

--- include/footer.php
+++ include/footer.php
@@ -4,4 +4,5 @@
     <a href="feed.php?category=friends"><i class="bi bi-people"></i></a>
     <a href="feed.php?category=chat-friends"><i class="bi bi-chat"></i></a>
     <div class="position-indicator <?php echo $indicatorClass ?>"></div>
+    <div class="fix-test"></div>
 </footer>
